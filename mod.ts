adc50d0779442ecf4138324df318868a243c4964
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-05-07 23:50:31 +0900

    polyfill

--- mod.ts
+++ mod.ts
@@ -4,6 +4,23 @@
 // AsyncIterator.next called, then call ReadableStream(1).pull.
 // ReadableStream(1) and WritableStream are buffered by TransformStream.
 
+// polyfill for ReadableStream.prototype[Symbol.asyncIterator]
+// https://bugs.chromium.org/p/chromium/issues/detail?id=929585#c10
+if (typeof ReadableStream.prototype[Symbol.asyncIterator] !== "function") {
+  ReadableStream.prototype[Symbol.asyncIterator] = async function* () {
+    const reader = this.getReader();
+    try {
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) return;
+        yield value;
+      }
+    } finally {
+      reader.releaseLock();
+    }
+  };
+}
+
 export type JSONValue =
   | { [key: string]: JSONValue }
   | JSONValue[]
